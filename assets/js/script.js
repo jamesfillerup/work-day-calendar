4b2d0fe204e1bcbe5271d11a8ea47cbce3ad38b7
jamesfill <jamesp.fillerup@gmail.com>
2022-03-15 10:43:32 -0600

    needs color attached to time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,17 @@
 // 1.1 pull data from api
 // 1.2 have it pull todays date
 
-// 2.1 hours from 9-5 show up on the page
-// 2.2 organize them into a col on the left 
+// 2.1 hours from 9-5 show up on the page (in html)
+// 2.2 organize them into a col on the left (in html)
 
-// 3.1 make text box
-// 3.2 make a col for text box 
+// 3.1 make text box(in html)
+// 3.2 make a col for text box (in html)
 // 3.3 text box color changes with the time of day 
 // 3.4 text does not get deleted when the page is refreshed
-// 3.5 text box can be edited
+// 3.5 text box can be edited (in html)
 
-// 4.1 save or lock symbol in the box 
-// 4.2 when clicked the text can't be deleted in the text box
+// 4.1 save or lock symbol in the box (in html)
+// 4.2 when clicked the text can't be deleted in the text box 
 
 
 //1.2 todays date
@@ -25,3 +25,25 @@ var m = moment().format('dddd, MMMM Do');
 
 var todaysDate = document.getElementById("currentDay").innerHTML = m.toString();
 
+//3.3 text box color changes with the time of day see classes as they have the colors in css
+
+function textBgColor(){
+    //This section of code came from source below
+    var hour = $(".hour").text().trim();
+
+    var time = moment(hour, "LT")
+    console.log(time);
+
+    $(".hour").removeClass(".present .past .future");
+
+    if(moment().isAfter(time)){
+        $(".hour").addClass("past");
+    }
+    else if (moment().isBefore(time)) {
+        $(".hour").addClass(".future");
+    } else {
+        $(".hour").addClass(".present");
+    }
+}
+//Source https://www.codegrepper.com/code-examples/javascript/how+to+use+moment+to+compare+time+for+calendar+change+color
+textBgColor();
